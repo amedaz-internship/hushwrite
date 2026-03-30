4489493a8d83a7729b30f5df04dbb196258d09e5
Elissa <elissatenn15@gmail.com>
2026-03-30 10:16:43 +0300

    implement PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+registerSW({ immediate: true });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
