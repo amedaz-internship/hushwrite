161acd8a4cacda0d9d04e8a34070ce444ff14e10
Elissa <elissatenn15@gmail.com>
2026-04-07 09:40:52 +0300

    fixed sanitization issue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mc4kqu27k3"
+    "revision": "0.97eckl7tdpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,10 +1,14 @@
+import DOMPurify from "dompurify";
+
 const Preview = ({ markdown }) => {
+  const cleanHTML = DOMPurify.sanitize(markdown);
+
   return (
     <div className="preview">
       <h3>Preview</h3>
       <div
         className="markdown-preview ck-content"
-        dangerouslySetInnerHTML={{ __html: markdown }}
+        dangerouslySetInnerHTML={{ __html: cleanHTML }}
       />
     </div>
   );
